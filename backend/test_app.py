7fe580d88da8bc4a697b5f3962116441cd305f0f
Hero <seiver@seznam.cz>
2022-04-29 21:29:48 +0200

    přidané testy

--- backend/test_app.py
+++ backend/test_app.py
@@ -10,3 +10,13 @@ def test_get_time():
 def test_get_time():
     """test FALSE statement"""
     assert app.get_time("nothing new") == "Null"
+
+
+def test_get_name():
+    """tests FALSE statement"""
+    assert app.get_name("nothing really") == "Null"
+
+
+def test_get_name():
+    """tests TRUE statement"""
+    assert app.get_name("1") == "appBot"
